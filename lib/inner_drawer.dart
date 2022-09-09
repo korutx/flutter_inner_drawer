1e5e47a72f60db0399698962c92bc0d42d580ca6
Michel David <mdavid.cu@gmail.com>
2022-09-08 22:31:40 -0400

    remove WidgetsBinding.instance! warning

--- lib/inner_drawer.dart
+++ lib/inner_drawer.dart
@@ -252,7 +252,7 @@ class InnerDrawerState extends State<InnerDrawer>
 
   /// get width of screen after initState
   void _updateWidth() {
-    WidgetsBinding.instance!.addPostFrameCallback((_) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
       final RenderBox? box =
           _drawerKey.currentContext!.findRenderObject() as RenderBox?;
       //final RenderBox box = context.findRenderObject();
